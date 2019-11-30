5a538f54fdfda9e1c7a6361c6b4e67369560f88f
matthew-hub <mateuszr430@gmail.com>
2019-11-30 21:36:10 +0100

    changed default props

--- utils/WindowCreate.js
+++ utils/WindowCreate.js
@@ -5,10 +5,10 @@ const { BrowserWindow } = require('electron');
 // default windows settings
 const defaultProps = {
   width: 1200,
-  width: 1200,
-  height: 900,
+  height: 800,
   minWidth: 800,
   minHeight: 600,
+  center: true,
   frame: false,
   show: false,
   backgroundColor: '#212121',
@@ -19,13 +19,25 @@ const defaultProps = {
   }
 };
 
-class WindowCreate extends BrowserWindow {
-  constructor({ file, ...windowSettings }) {
-    // console.log(windowSettings);
-    // console.log(defaultProps);
-    super({ ...defaultProps, ...windowSettings });
 
-    this.loadFile('renderer/' + file);
+class WindowCreate extends BrowserWindow {
+  constructor(file, windowSettings) {
+    // console.log(windowSettings);
+    // console.log('[FILE]', file.file);
+
+    // // const fileURL = ...file;
+    // // console.log(defaultProps);
+    const settings = {
+      ...defaultProps,
+      ...windowSettings
+    };
+    const fileURL = file.file;
+    super({ ...settings });
+    // console.log(this);
+    // console.log('[settings]', settings);
+
+    this.loadFile(fileURL);
+    // this.loadURL(fileURL);
     this.webContents.openDevTools();
 
     this.once('ready-to-show', () => {
